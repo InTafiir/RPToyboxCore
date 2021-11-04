f9d3be96cd6e5c3746aaf00fd0249f6387baebfb
InTafiir <InTafiir@gmail.com>
2021-11-04 17:00:07 -0500

    * Added onEnable / onDisable in Main

--- src/main/java/net/finalbarrage/RPToyboxCore/Main.java
+++ src/main/java/net/finalbarrage/RPToyboxCore/Main.java
@@ -3,4 +3,15 @@ package net.finalbarrage.RPToyboxCore;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
 }
