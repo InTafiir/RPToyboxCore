c85867fed6dc83be7ce37357375652f0553cae70
InTafiir <InTafiir@gmail.com>
2021-11-04 16:41:38 -0500

    * Extends Javaplugin in Main.java * Converted project to Maven Framework * Added spigot api

---
+++ src/main/java/net/finalbarrage/RPToyboxCore/Main.java
@@ -0,0 +1,6 @@
+package net.finalbarrage.RPToyboxCore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+}
--- src/net/finalbarrage/RPToyboxCore/Main.java
+++
@@ -1,4 +0,0 @@
-package net.finalbarrage.RPToyboxCore;
-
-public class Main {
-}
