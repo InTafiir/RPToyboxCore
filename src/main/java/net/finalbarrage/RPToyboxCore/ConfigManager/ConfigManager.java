75f9d0df2c9b5872053f704707471bb465792b47
Logan Miller <logan@millercyber.tech>
2021-11-04 16:51:26 -0500

    Added ConfigManager

---
+++ src/main/java/net/finalbarrage/RPToyboxCore/ConfigManager/ConfigManager.java
@@ -0,0 +1,11 @@
+package net.finalbarrage.RPToyboxCore.ConfigManager;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+
+public class ConfigManager extends JavaPlugin {
+    private File configFile;
+    private FileConfiguration config;
+}
